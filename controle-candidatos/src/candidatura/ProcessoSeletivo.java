e33bf93dd9df14c9edcf50fa2cef1e2c009eb3fb
Kleber <klebercrf14@gmail.com>
2025-05-09 17:21:35 -0300

    analisando os candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,4 +1,19 @@
 package candidatura;
 
 public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCanditado(1900.0);
+        analisarCanditado(2200.0);
+        analisarCanditado(2000.0);
+    }
+        static void analisarCanditado(double salarioPretendido) {
+            double salarioBase = 2000.0;
+            if(salarioBase > salarioPretendido) {
+                System.out.println("LIGAR PARA O CANDITADO");
+            }else if (salarioBase == salarioPretendido)
+                System.out.println("LIGAR PARA O CANDITADO COM CONSTRAPROPOSTA");
+            else {
+                System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+            }
+        }
 }
